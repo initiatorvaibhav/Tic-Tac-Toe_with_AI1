c57959f7f80358a8b7bbd1de33da0150528c5fd3
Vaibhav <vaibhav.singh_cs18@gla.ac.in>
2020-07-15 00:19:55 +0530

    two for loop for current readable view

--- Tic-Tac-Toe with AI/task/src/tictactoe/Main.java
+++ Tic-Tac-Toe with AI/task/src/tictactoe/Main.java
@@ -7,6 +7,17 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         char[][] array = new char[5][9];
         String str = scanner.next();
-
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (i == 0 || i == 4)
+                    array[i][j] = '-';
+                else if ((i != 0 && i != 4 && j == 0) || (i != 0 && i != 4 && j == 8))
+                    array[i][j] = '|';
+                else if (i > 0 && j % 2 != 0)
+                    array[i][j] = ' ';
+                else
+                    array[i][j] = ' ';
+            }
+        }
     }
 }
